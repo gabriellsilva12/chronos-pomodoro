931796df9e8a1ef90f794d674e77caf379140954
Gabriel Silva <gsilva04950@gmail.com>
2026-01-08 17:02:51 -0300

    feat: use props and children in components

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ import './styles/global.css';
 export function App() {
   return (
     <div>
-      <Heading />
+      <Heading>Olá mundo 1!</Heading>
+      <Heading>Olá mundo 2!</Heading>
+      <Heading>Olá mundo 3!</Heading>
+      <Heading attr={4321}>Olá mundo !</Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aliquam
         fuga in dignissimos, commodi suscipit veritatis tempora quae ea quisquam
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,5 @@
 import styles from './Heading.module.css';
 
-export function Heading() {
-  return <h1 className={styles.cyan}>Hello World!</h1>;
+export function Heading(props) {
+  return <h1 className={styles.cyan}>{props.children} {props.attr ? props.attr: null}</h1>;
 }
