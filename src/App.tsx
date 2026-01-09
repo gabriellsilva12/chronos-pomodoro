16b69649176e5ca3a5f37ed13d07d8016c99a804
Gabriel Silva <gsilva04950@gmail.com>
2026-01-08 21:19:04 -0300

    style: add CSS variables and styles global to chronos

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ export function App() {
   return (
     <div>
       <Heading>Olá mundo 1!</Heading>
-      <Heading>Olá mundo 2!</Heading>
-      <Heading>Olá mundo 3!</Heading>
-      <Heading attr={4321}>Olá mundo !</Heading>
+
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aliquam
         fuga in dignissimos, commodi suscipit veritatis tempora quae ea quisquam
