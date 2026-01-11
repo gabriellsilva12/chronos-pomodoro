82be4e91a2d7e0789d7e22aa1cdaf380f79424d8
Gabriel Silva <gsilva04950@gmail.com>
2026-01-10 21:01:35 -0300

    feat: create initial page form and add input component with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 import { CountDown } from './components/CountDown'
+import { DefaultInput } from './components/DefaultInput'
+
 import './styles/theme.css';
 import './styles/global.css';
 
@@ -23,7 +24,24 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading> FOOTER </Heading>
+        <form className='form' action="">
+          <div className="row">
+            <DefaultInput id={'meuInput'} type={'text'}/>
+          </div>
+
+          <div className="row">
+            <p>Nesse ciclo <strong>descanse</strong> por <strong>5 minutos</strong></p>
+          </div>
+
+          <div className="row">
+            <p>Ciclos</p>
+            <p> 0 0 0 0 </p>
+          </div>
+
+          <div className="row">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
