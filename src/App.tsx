250e0c8bca9e0966b12edfd300d1cbde07a1a4cf
Gabriel Silva <gsilva04950@gmail.com>
2026-01-13 15:08:38 -0300

    feat: create footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { PlayCircleIcon } from 'lucide-react';
 
 import { Container } from './components/Container';
-import { Logo } from './components/Logo'
-import { Menu } from './components/Menu'
-import { CountDown } from './components/CountDown'
-import { DefaultInput } from './components/DefaultInput'
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
+import { Footer } from './components/Footer';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -15,40 +16,47 @@ export function App() {
   return (
     <>
       <Container>
-        <Logo/>
-      </Container>
-      
-      <Container>
-        <Menu/>
+        <Logo />
       </Container>
 
       <Container>
-        < CountDown />
+        <Menu />
       </Container>
 
       <Container>
-        <form className='form' action="">
-          <div className="row">
-            <DefaultInput 
-            id={'meuInput'} 
-            type={'text'} 
-            labelText={'tasks'} 
-            title='title' 
-          />
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='row'>
+            <DefaultInput
+              id={'meuInput'}
+              type={'text'}
+              labelText={'tasks'}
+              title='title'
+            />
           </div>
 
-          <div className="row">
-            <p>Nesse ciclo <strong>descanse</strong> por <strong>5 minutos</strong></p>
+          <div className='row'>
+            <p>
+              Nesse ciclo <strong>descanse</strong> por{' '}
+              <strong>5 minutos</strong>
+            </p>
           </div>
 
-          <div className="row">
-            <Cycles/>
+          <div className='row'>
+            <Cycles />
           </div>
 
-          <div className="row">
-            < DefaultButton icon={ <PlayCircleIcon/>} color='green'/>
+          <div className='row'>
+            <DefaultButton icon={<PlayCircleIcon />} color='green' />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
